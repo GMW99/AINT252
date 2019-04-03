a2343ad7005cbadbb0900fd09c93c167af6fdb30
Gabryel Mason-Williams <13510038+GMW99@users.noreply.github.com>
2019-04-03 09:27:21 +0100

    Added Histograms

--- Coursework/kmeans.m
+++ Coursework/kmeans.m
@@ -1,3 +1,13 @@
 % K-means clustering (unsupervised learning)
 data = gen_kmeansdata(10574626);
+columnMeans = mean(data);
+stdOfData = std(data);
+figure(1);
+histogram(X(:,1));
+figure(2);
+histogram(X(:,2));
+figure(3);
+histogram(X(:,3));
+figure(4);
+histogram(X(:,4));
 
